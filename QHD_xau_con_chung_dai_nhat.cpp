57c6d77175922b12b05eb0a9ec9babb266ee39ce
Luong Chung <chunglv42@wru.vn>
2018-01-11 16:54:47 +0700

    Update QHD_xau_con_chung_dai_nhat.cpp

--- QHD_xau_con_chung_dai_nhat.cpp
+++ QHD_xau_con_chung_dai_nhat.cpp
@@ -1,48 +1,56 @@
-#include <iostream>
-#include <string>
+#include<iostream>
 using namespace std;
-string w, s;
-int dequy(int m, int n) {
-  if ((m == 0) || (n == 0)) return 0;
-  if (w[m-1] == s[n-1]) return 1 + dequy(m-1, n-1);
-  return max(dequy(m-1, n),dequy(m, n-1));
-}
-int a[50][50];
-void quyhoachdong(int m, int n) {
-  for (int i = 0; i <= n; i++) a[0][i] = 0;
-  for (int i = 1; i <= m; i++) {
-    a[i][0] = 0;
-    for (int j = 1; j <= n; j++)
-      if (w[i-1] == s[j-1]) a[i][j] = 1 + a[i-1][j-1];
-      else a[i][j] = max(a[i-1][j], a[i][j-1]);
-  }
-}
-void inA(int m, int n) {
-  for (int i = 0; i <= m; i++) {
-    for (int j = 0; j <= n; j++)
-      cout << a[i][j] << " ";
-    cout << endl;
-  }
-}
-void inXauCon(int m, int n) {
-  if ((m == 0) || (n == 0)) return;
-  if (w[m-1] == s[n-1]) {
-    inXauCon(m-1, n-1);
-    cout << w[m-1];
-    return;
-  }
-  if (a[m][n] == a[m-1][n]) inXauCon(m-1, n);
-  else inXauCon(m, n-1);
+string s1="AQSRWRTXEDDY",s2="AEQWSRRSFXTGDD";
+int A[1000][1000];
+char vt[1000][1000];
+void quyhoachdong() {
+    for (int j = 0; j <= s1.length(); ++j) A[0][j] = 0;
+    for (int i = 1; i <= s2.length(); ++i) {
+        A[i][0] = 0;
+        for (int j = 1; j <= s1.length(); ++j) {
+            if (s2[i - 1] == s1[j - 1]) {
+                A[i][j] = A[i - 1][j - 1] + 1;
+                vt[i][j] = 'c';
+
+            } else {
+                A[i][j] = max(A[i - 1][j], A[i][j - 1]);
+                vt[i][j] = (A[i - 1][j] > A[i][j - 1]) ? 'l' : 't';
+            }
+        }
+    }
 }
 
-int main() {
-  cout << "W = "; getline(cin, w);
-  cout << "S = "; getline(cin, s);
-  //tính dộ dài dãy con chung bằng đệ quy
-  cout << "Do dai day con chung dai nhat = " << dequy(w.length(), s.length()) << endl;
-  //tính bằng quy hoạch động
-  quyhoachdong(w.length(), s.length());
-  inA(w.length(), s.length());
-  //truy vết
-  inXauCon(w.length(), s.length());
+void hienbang(){
+    cout<<endl;
+    for (int i = 0; i <= s2.length(); ++i) {
+        for (int j = 0; j <=s1.length(); ++j) {
+            cout<<A[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    for (int i = 0; i <= s2.length(); ++i) {
+        for (int j = 0; j <=s1.length(); ++j) {
+            cout<<vt[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+void truyvet(int i,int j){
+    if(i<1) return;
+    if(j<1) return;
+    if(vt[i][j]=='c'){
+        truyvet(i-1,j-1);
+        cout<<s2[i-1];
+    } else if(vt[i][j]=='l'){
+        truyvet(i-1,j);
+    } else{
+        truyvet(i,j-1);
+    }
+}
+int main(){
+    quyhoachdong();
+    hienbang();
+    cout<<endl<<"Ket qua: ";
+    truyvet(s2.length(),s1.length());
 }
